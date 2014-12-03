55d8c3f8dee9d9990e01f53386e581a077df4b6f
unknown <rmork@MD-RMORK8.taylormadefarm.local>
2014-12-03 16:43:10 -0500

    Index Page Mods

--- app/views/public/index.blade.php
+++ app/views/public/index.blade.php
@@ -21,37 +21,41 @@
                       <img src="/img/slide1.jpg" alt="Slide 1">
 
                     </div>
+                    <div class="item">
+                      <img src="/img/slide1.jpg" alt="Slide 1">
+
+                    </div>
 
 
                   </div>
 
-                  {{--<!-- Controls -->--}}
-                  {{--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">--}}
-                    {{--<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>--}}
-                    {{--<span class="sr-only">Previous</span>--}}
-                  {{--</a>--}}
-                  {{--<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">--}}
-                    {{--<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>--}}
-                    {{--<span class="sr-only">Next</span>--}}
-                  {{--</a>--}}
+                  <!-- Controls -->
+                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
                 </div>
 
                 <div class="row important-links">
 
 
-                <div class="col-md-15 col-sm-3">
-                    <img  src="/img/storylogo.png" alt="Our Story"/>
-                </div>
-                <div class="col-md-15 col-sm-3">
+                {{--<div class="col-md-3">--}}
+                    {{--<img  src="/img/storylogo.png" alt="Our Story"/>--}}
+                {{--</div>--}}
+                <div class="col-md-3">
                     <img  src="/img/speciallogo.png" alt="Weekly Specials"/>
                 </div>
-                <div class="col-md-15 col-sm-3">
+                <div class="col-md-3">
                     <img  src="/img/caterlogo.png" alt="Let Us Cater"/>
                 </div>
-                <div class="col-md-15 col-sm-3">
+                <div class="col-md-3">
                     <img  src="/img/menulogo.png" alt="Menu"/>
                 </div>
-                <div class="col-md-15 col-sm-3">
+                <div class="col-md-3">
                     <img  src="/img/applogo.png" alt="Download Our App"/>
                 </div>
                 <div class="clearfix"></div>
