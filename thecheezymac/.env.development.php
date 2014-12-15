eb46633772b4ee4b04b350b96880af4dedd6f100
s34p <s34p@users.noreply.github.com>
2014-12-15 14:07:56 -0500

    untracking env files

--- thecheezymac/.env.development.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'DB_NAME' => 'thecheezymac',
-    'DB_USERNAME' => 'admin',
-    'DB_PASSWORD' => 'password',
-    'EMAIL_ADDRESS'=>'jerry@thecheezymac.com',
-    'EMAIL_PASSWORD' =>'CheezyMac12!',
-];
