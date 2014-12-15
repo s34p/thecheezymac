8aa76b23533199eb5e9c6f247308bd864ff5f5c7
unknown <rmork@MD-RMORK8.taylormadefarm.local>
2014-12-15 17:10:29 -0500

    Final Edits

--- thecheezymac/app/views/public/story.blade.php
+++ thecheezymac/app/views/public/story.blade.php
@@ -50,15 +50,15 @@
 
                                                   </div>
 
-                                                  {{--<!-- Controls -->--}}
-                                                  {{--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">--}}
-                                                    {{--<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>--}}
-                                                    {{--<span class="sr-only">Previous</span>--}}
-                                                  {{--</a>--}}
-                                                  {{--<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">--}}
-                                                    {{--<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>--}}
-                                                    {{--<span class="sr-only">Next</span>--}}
-                                                  {{--</a>--}}
+                                                  <!-- Controls -->
+                                                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                                                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                                    <span class="sr-only">Previous</span>
+                                                  </a>
+                                                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                                                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                                    <span class="sr-only">Next</span>
+                                                  </a>
                                                 </div>
                             </div>
                             <div class="col-md-8 storytext">
